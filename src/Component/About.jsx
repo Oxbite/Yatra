c5ec66ef26500959496e7fedc612e24634444946
aoz69 <aozpokharel@gmail.com>
2023-03-28 15:53:17 +0545

    about us updated with animation

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -8,22 +8,34 @@ export default function About(){
     return(
         <div>
             <div className="abutus" id='about'>
+                
+
                 <div className="abtcontainer">
+
+                        
+                        <object type="image/svg+xml" aria-label="pcImage" data={book} width="70px" className='book'></object>
+                        <object type="image/svg+xml" aria-label="pcImage" data={book} width="70px" className='book'></object>
+                        <object type="image/svg+xml" aria-label="pcImage" data={book} width="70px" className='book'></object>
+                        <object type="image/svg+xml" aria-label="pcImage" data={book} width="70px" className='book'></object>
+                        <object type="image/svg+xml" aria-label="pcImage" data={book} width="70px" className='book'></object>
+
+
+
                     <div className="abtimg">
                         <ScrollRotate method={"perc"} loops={3}>
                             <object type="image/svg+xml" aria-label="pcImage" data={book} width="70px"></object>
-                            {/* <object type="image/svg+xml" aria-label="pcImage" data={book} width="70px"></object> */}
+                            
                         </ScrollRotate>
                     </div>
 
                     <p className='heading'> ABOUT <span>YATRA</span></p>
                     <p className='abuttext'>
-                        Yatra is so cool so you are my client. <br />
-                        Yatra is so cool so you are my client. <br />
-                        Yatra is so cool so you are my client. <br />
-                        Yatra is so cool so you are my client. <br />
-                        Yatra is so cool so you are my client. <br />
-                        Yatra is so cool so you are my client. <br />
+                    Yatra is a dynamic and innovative web development company that specializes in creating custom websites for clients.
+                    With a focus on delivering high-quality websites that meet the unique needs and preferences of each client,
+                    Whether you come to Yatra with their own designs or rely on the company's expert team to design a website from scratch,
+                    you can expect a seamless and personalized experience
+                    Yatra's commitment to customer satisfaction and cutting-edge web development techniques has made it a trusted partner for
+                    businesses of all sizes. 
                     </p>
                 </div>
             </div>
