098b90687771d5e5aa04fd06106d3875e1037483
aoz69 <aozpokharel@gmail.com>
2023-03-28 14:19:28 +0545

    ROTATION to start working

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -12,9 +12,10 @@ export default function About(){
                     <div className="abtimg">
                         <ScrollRotate method={"perc"} loops={3}>
                             <object type="image/svg+xml" aria-label="pcImage" data={book} width="70px"></object>
+                            {/* <object type="image/svg+xml" aria-label="pcImage" data={book} width="70px"></object> */}
                         </ScrollRotate>
-
                     </div>
+
                     <p className='heading'> ABOUT <span>YATRA</span></p>
                     <p className='abuttext'>
                         Yatra is so cool so you are my client. <br />
