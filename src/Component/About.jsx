9f1e29ceb350abe83e27118615e30af974717cea
aoz69 <aozpokharel@gmail.com>
2023-03-28 13:28:53 +0545

    about us book roatates 24/7

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Imageanimation from './Imageanimation';
-import book from '../images/images/book.svg';
+import book from '../images/images/aboutBook.svg';
 
 // import aboutimg from '../images/images/aboutBook.png';
 
@@ -11,10 +11,7 @@ export default function About(){
             <div className="abutus" id='about'>
                 <div className="abtcontainer">
                     <div className="abtimg">
-                        {/* <img src={aboutimg} width="100px" height="100px" /> */}
-                        <object type="image/svg+xml" aria-label="pcImage" data={book} width="100%"></object>
-
-                        {/* <Imageanimation/> */}
+                        <object type="image/svg+xml" aria-label="pcImage" data={book} width="70px"></object>
                     </div>
                     <p className='heading'> ABOUT <span>YATRA</span></p>
                     <p className='abuttext'>
